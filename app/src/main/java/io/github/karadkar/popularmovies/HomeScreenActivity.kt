ae15b49f161aff35df569629805de160c63eb0d2
Rohit Karadkar <karadkar.rohit@gmail.com>
2018-08-15 15:00:59 +0530

    new project setup

---
+++ app/src/main/java/io/github/karadkar/popularmovies/HomeScreenActivity.kt
@@ -0,0 +1,12 @@
+package io.github.karadkar.popularmovies
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class HomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
