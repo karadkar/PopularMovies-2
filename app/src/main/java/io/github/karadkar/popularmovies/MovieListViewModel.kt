7814bc0357e8178c38a33017a9cfc02d101d0216
Rohit Karadkar <karadkar.rohit@gmail.com>
2018-08-18 10:40:47 +0530

    annotation to open kotlin classes for testing only in debug mode. classes will remain final in release

--- app/src/main/java/io/github/karadkar/popularmovies/MovieListViewModel.kt
+++ app/src/main/java/io/github/karadkar/popularmovies/MovieListViewModel.kt
@@ -2,7 +2,9 @@ package io.github.karadkar.popularmovies
 
 import android.arch.lifecycle.ViewModel
 import io.github.karadkar.popularmovies.data.MovieListRepository
+import io.github.karadkar.popularmovies.testing.OpenForTesting
 
+@OpenForTesting
 class MovieListViewModel(
         val respository: MovieListRepository,
         val genre: String
