ba2321a22ee9a6d1857ac2f7d471e9d40b4ad050
Rohit Karadkar <karadkar.rohit@gmail.com>
2018-08-20 22:11:08 +0530

    added test app module

--- app/src/androidTest/java/io/github/karadkar/popularmovies/TestApp.kt
+++ app/src/androidTest/java/io/github/karadkar/popularmovies/TestApp.kt
@@ -1,12 +1,13 @@
 package io.github.karadkar.popularmovies
 
 import android.app.Application
+import org.koin.android.ext.android.startKoin
 
 // needs testInstrumentationRunner "android.support.test.runner.AndroidJUnitRunner" to defaultConfig
 // Application class for Instrumentation test
 class TestApp : Application() {
     override fun onCreate() {
         super.onCreate()
-//        startKoin(context = this, modules = testAppModules)
+        startKoin(context = this, modules = listOf())
     }
 }
--- app/src/androidTest/java/io/github/karadkar/popularmovies/di/TestAppModules.kt
+++ app/src/androidTest/java/io/github/karadkar/popularmovies/di/TestAppModules.kt
@@ -3,6 +3,7 @@ package io.github.karadkar.popularmovies.di
 
 import io.github.karadkar.popularmovies.MovieListViewModel
 import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.Module
 import org.koin.dsl.module.module
 import org.mockito.Mockito.mock
 
@@ -13,4 +14,4 @@ private val testMovieListModule = module {
     }
 }
 
-val testAppModules = listOf(testMovieListModule)
+val testAppModules = listOf<Module>()
