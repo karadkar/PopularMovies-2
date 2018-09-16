15f95b07cb2a4a1f0c693844074646342bff87dd
Rohit Karadkar <karadkar.rohit@gmail.com>
2018-09-16 19:04:30 +0530

    made dry-run test in robolectric

--- app/src/test/java/io/github/karadkar/popularmovies/KoinDryRunTest.kt
+++ app/src/test/java/io/github/karadkar/popularmovies/KoinDryRunTest.kt
@@ -2,9 +2,14 @@ package io.github.karadkar.popularmovies
 
 import io.github.karadkar.popularmovies.data.appModules
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.koin.test.KoinTest
 import org.koin.test.checkModules
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE, sdk = [23])
 class KoinDryRunTest : KoinTest {
     @Test
     fun dependencyGraphDryRun() {
